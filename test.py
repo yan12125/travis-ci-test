b0cd52940abe33e0ea6a283e564194dce93860ad
Yen Chi Hsuan <yan12125@gmail.com>
2016-01-02 06:06:50 +0800

    Copy string writing functions from youtube-dl

--- test.py
+++ test.py
@@ -1,10 +1,44 @@
 # coding: utf-8
+from __future__ import unicode_literals
 import unittest
+
+import locale
+import sys
+
+
+def preferredencoding():
+    """Get preferred encoding.
+
+    Returns the best encoding scheme for the system, based on
+    locale.getpreferredencoding() and some further tweaks.
+    """
+    try:
+        pref = locale.getpreferredencoding()
+        'TEST'.encode(pref)
+    except Exception:
+        pref = 'UTF-8'
+
+    return pref
+
+
+def write_string(s, out=sys.stdout, encoding=None):
+    if ('b' in getattr(out, 'mode', '') or
+            sys.version_info[0] < 3):  # Python 2 lies about mode of sys.stderr
+        byt = s.encode(encoding or preferredencoding(), 'ignore')
+        out.write(byt)
+    elif hasattr(out, 'buffer'):
+        enc = encoding or getattr(out, 'encoding', None) or preferredencoding()
+        byt = s.encode(enc, 'ignore')
+        out.buffer.write(byt)
+    else:
+        out.write(s)
+    out.flush()
 
 
 class TestFoo(unittest.TestCase):
     def test_failed(self):
-        print('中文')
+        write_string('中文')
+
         raise Exception('中文')
 
 if __name__ == '__main__':
